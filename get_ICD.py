820f9335c0bab594613f8b59320f03df4db616cb
James Jensen <jamesronaldjensen@gmail.com>
2019-03-16 14:17:59 -0500

    added function to make dictionary mapping codes to diagnoses

--- get_ICD.py
+++ get_ICD.py
@@ -44,12 +44,32 @@ def translate_general(code):
 
     rv = soup.find('div').next_sibling.next_sibling.find('div', class_='searchPadded')
 
-    if not rv:
+    if not rv.text:
         print(code)
     else:
         rv = rv.text
 
     return rv
+
+def make_dictionary(df):
+
+    icd_dictionary = {}
+    # new_df = df['DIAGNOSIS_SHORT_1'].apply(lambda x: x.strip('-')).to_frame()
+    bad_diagnoses = ["V990", "V991", "V992", "V997", "-9", "V99","V97"]
+    df.query("DIAGNOSIS_SHORT_1 not in @bad_diagnoses", inplace=True)
+    new_df = df['DIAGNOSIS_SHORT_1'].to_frame()
+    new_df = new_df.dropna(subset=['DIAGNOSIS_SHORT_1'])
+
+    for row in new_df.itertuples(index=True, name='Pandas'):
+        code = getattr(row, "DIAGNOSIS_SHORT_1")
+        if code:
+            if code not in icd_dictionary.keys():
+                icd_dictionary[code] = translate_general(code)
+
+    return icd_dictionary
+
+
+
 
 
 # top['DIAG1'] = top['DIAG1'].apply(lambda x: get_ICD.translate_code(x))

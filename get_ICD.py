0641380a41815ea60aea0b602dc3c480d555eb00
James Jensen <jamesronaldjensen@gmail.com>
2019-03-15 22:32:54 -0500

    edited ICD scraper so that it gets diagnoses for short codes

--- get_ICD.py
+++ get_ICD.py
@@ -6,41 +6,50 @@ import bs4
 
 
 BASE = 'https://icdcodelookup.com/icd-10/codes/'
-BASE_GEN = 'http://icd9.chrisendres.com/index.php?srchtype=diseases&srchtext='
+# BASE_GEN = 'http://icd9.chrisendres.com/index.php?srchtype=diseases&srchtext='
+BASE_GEN = 'https://www.icd10data.com/search?s='
 COUNT = 0
 
 
 
 def translate_code(code):
 
-	url = BASE + str(code)
-	ro = util.get_request(url)
-	html = util.read_request(ro)
-	soup = bs4.BeautifulSoup(html, "html5lib")
+    url = BASE + str(code)
+    ro = util.get_request(url)
+    html = util.read_request(ro)
+    soup = bs4.BeautifulSoup(html, "html5lib")
 
-	rv = soup.find('h3', class_='bold')
+    rv = soup.find('h3', class_='bold')
 
-	if not rv:
-		print(code)
-	else:
-		rv = rv.get_text().strip()
+    if not rv:
+        print(code)
+    else:
+        rv = rv.get_text().strip()
 
-	# lst = soup.find('div', class_="codeSearchContainer").find_all('script')
-	# txt = lst[2].get_text() 
-	# print(txt)
-	# rv = re.search(r'pageTitle = "(.*?)ICD', txt).group(1).strip()[8:]
+    # lst = soup.find('div', class_="codeSearchContainer").find_all('script')
+    # txt = lst[2].get_text() 
+    # print(txt)
+    # rv = re.search(r'pageTitle = "(.*?)ICD', txt).group(1).strip()[8:]
 
-	return rv
+    return rv
 
 def translate_general(code):
 
-	url = BASE_GEN + str(code) + '&Submit=Search&action=search'
-	ro = util.get_request(url)
-	html = util.read_request(ro)
-	soup = bs4.BeautifulSoup(html, "html5lib")
-	txt = soup.find('div', class_='dlvl').text 
+    # url = BASE_GEN + str(code) + '&Submit=Search&action=search'
+    url = BASE_GEN + str(code) + '&codebook=icd9volume1'
+    ro = util.get_request(url)
+    html = util.read_request(ro)
+    soup = bs4.BeautifulSoup(html, "html5lib")
+    # txt = soup.find('div', class_='dlvl')
 
-	return txt
+    rv = soup.find('div').next_sibling.next_sibling.find('div', class_='searchPadded')
+
+    if not rv:
+        print(code)
+    else:
+        rv = rv.text
+
+    return rv
 
 
 # top['DIAG1'] = top['DIAG1'].apply(lambda x: get_ICD.translate_code(x))

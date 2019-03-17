0de5a3ca39982a2a36231ff2193c6b780da8ac46
James Jensen <jamesronaldjensen@gmail.com>
2019-03-16 19:13:13 -0500

    added go long function to break symptom string into long data and merge back into dataframe

--- process.py
+++ process.py
@@ -7,7 +7,8 @@ import numpy as np
 import json
 
 DIAGNOSIS_PATH = "data/icd_codes.json"
-INPUT_FNAME = 'data/d.csv'
+# INPUT_FNAME = 'data/d.csv'
+INPUT_FNAME = '/Users/jamesjensen/Documents/harris/q2_18_19/final_project/mayo-symptom-tree/cdc_2012_2016_clean.csv'
 OUTPUT_FNAME = 'cleaned.csv'
 
 REPLACEMENT_DICT = {'AGE': {
@@ -18,7 +19,7 @@ REPLACEMENT_DICT = {'AGE': {
                     'VISIT_REASON_CAT': {"6": "Preventive care"},
                     'HEIGHT_INCHES': {
                         "72 inches (capped value for females)": "72",
-                        "77 inches (capped value for males": "77"},
+                        "77 inches (capped value for males)": "77"},
                     'WEIGHT_POUNDS': {"350 lbs. or more": "350"},
                     'STATE': {"71": np.nan, "72": np.nan, "73": np.nan,
                             "74": np.nan, "96": "ESC_Div_Remainder",
@@ -37,9 +38,12 @@ COL_NAMES = ['AGE', 'AGE_CAT', 'SEX', 'PREGNANT', 'RACE_ETHNICITY',
              'WEIGHT_POUNDS', 'TEMP_FAHRENHEIT', 'REGION',
              'CENSUS_DIVISION','STATE']
 
-BAD_DIAGNOSES = ["V990","V990-", "V991", "V992", "V997", "-9", "V99","V97",np.nan]
+BAD_DIAGNOSES = ["V990","V990-", "V991", "V992", "V997", "-9", "V99","V97", np.nan]
 BAD_INJURY = ["Yes"]
-BAD_SYMPTOMS = ["Blank"]
+BAD_SYMPTOMS = ["Blank", "visit", "medication", "counseling", "injury", "surgery"]
+VISIT_REASON_COL = 'VISIT_REASON_1'
+DIAGNOSIS_COL = 'DIAGNOSIS_SHORT_1'
+LONG_NAMES = ['VISIT_REASON_1', 'SYMP1','SYMP2','SYMP3','SYMP3', 'SYMP4']
 
 def get_diagnosis_map():
     '''
@@ -78,7 +82,32 @@ def read_and_process_data():
         lambda x: str(x).strip('-'))
     df['DIAGNOSIS_SHORT_1'] = df['DIAGNOSIS_SHORT_1'].apply(lambda x: dm[x])
 
-    df.to_csv(OUTPUT_FNAME, index=False)
+    sorted_df = go_long(df)
+    df = pd.merge(df, sorted_df, on=VISIT_REASON_COL, how='left')
+
+    df[VISIT_REASON_COL] = df[VISIT_REASON_COL].str.lower()
+    df[DIAGNOSIS_COL] = df[DIAGNOSIS_COL].str.lower()
+
+    # df.to_csv(OUTPUT_FNAME, index=False)
 
     return df
+
+
+def go_long(df):
+
+    new = df[VISIT_REASON_COL].str.split(',', expand = True)
+    new_df = df[VISIT_REASON_COL].to_frame().join(new) 
+    new_df.columns = LONG_NAMES
+
+    keys = [c for c in new_df if c.startswith('SYMP')]
+    melted_df = pd.melt(new_df, id_vars=VISIT_REASON_COL, value_vars=keys, value_name='KEY')
+    no_dupes = melted_df.drop_duplicates()
+
+    sorted_df = no_dupes.sort_values([VISIT_REASON_COL, 'variable'])
+    sorted_df.drop('variable', axis=1, inplace=True)
+    sorted_df.dropna(inplace=True)
+    sorted_df = sorted_df[~sorted_df['KEY'].str.contains('...', regex=False)]
+
+
+    return sorted_df
 

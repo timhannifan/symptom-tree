e8aedcff810cc48e89cef0d54f95b460e750ccf3
tglazer <tglazer@internationaldevelopmentgroup.com>
2019-03-18 16:35:24 -0500

    pylinting complete on all but main

--- pca.py
+++ pca.py
@@ -8,7 +8,7 @@ from sklearn.decomposition import PCA
 def test_pca(x_train, x_test):
     '''
     Implements sklearn packages for principle component analysis
-    
+
     Input:
     x_train (pandas df): training data
     x_test (pandas df): testing data
@@ -26,3 +26,4 @@ def test_pca(x_train, x_test):
     explained_variance = pca.explained_variance_ratio_
 
     return explained_variance
+    
